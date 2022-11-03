081942fef44fe215a9884bb74cdaab92473ac1eb
Югослав <115298811+Maidov@users.noreply.github.com>
2022-11-03 15:48:25 +0300

    Исправелно на двумерную матрицу + кодстайл

--- main.c
+++ main.c
@@ -1,69 +1,65 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
-#define maxN 100
+#define MAX_N 7
 
-int CustomGrade(int a, int b) {
-  int result = 1;
+int CustomPow(int a, int b) {
+    int result = 1;
 
-  for (int i = 0; i < b; i++) {
-    result *= a;
-  }
-  return result;
+    for (int i = 0; i < b; i++) {
+        result *= a;
+    }
+    return result;
 }
 
-
-
-
 int main() {
-  int lambda, dForDelta, matrix[maxN * maxN], n;
+    int lambda, dForDelta, matrix[MAX_N][MAX_N], n;
 
-  //Ввод данных
-  scanf("%i", &n);
+    //Ввод данных
+    scanf("%i", &n);
 
-  for (int i = 0; i != n*n; ++i) {
-      scanf("%d", &matrix[i]);
-  }
-
-  int alfa = n - 1, current = (n * n) - 1;
-
-  // Первая часть
-  printf("\n%d\n", matrix[current]);
-
-  for (int i = 0; i < alfa; i++) {  
-    lambda = i + 1;
-    dForDelta = CustomGrade(-1, i);
-
-    if (dForDelta > 0) {
-      current -= 1;
-    } else {
-      current -= n;
+    for (int i = 0; i != n; ++i) {
+        for (int j = 0; j != n; ++j)
+            scanf("%d", &matrix[i][j]);
     }
+    int alfa = n - 1, currentX = alfa, currentY = alfa;
 
-    printf("%d\n", matrix[current]);
+    // Первая часть
+    printf("\n%d ", matrix[currentY][currentX]);
 
-    for (int i = 0; i < lambda; i++) {  
-      current = current - alfa * dForDelta;
-      printf("%d\n", matrix[current]);
+    for (int i = 0; i < alfa; i++) {
+        lambda = i + 1;
+        dForDelta = CustomPow(-1, i);
+
+        if (dForDelta > 0) {
+            currentX -= 1;
+        } else {
+            currentY -= 1;
+        }
+        printf("%d ", matrix[currentY][currentX]);
+
+        for (int i = 0; i < lambda; i++) {
+            currentX = currentX + dForDelta;
+            currentY = currentY - dForDelta;
+            printf("%d ", matrix[currentY][currentX]);
+        }
     }
-  }
+    
+    //Вторая часть
+    for (int i = alfa; 0 < i; i--) {
+        lambda = i - 1;
+        if (dForDelta > 0) {
+            currentX -= 1;
+        } else {
+            currentY -= 1;
+        }
+        printf("%d ", matrix[currentY][currentX]);
 
-  //Вторая часть
-  for (int i = alfa; 0 < i; i--) {
-    lambda = i - 1;
-
-    if (dForDelta > 0) {
-      current -= 1;
-    } else {
-      current -= n;
+        for (int i = 0; i < lambda; i++) {
+            currentX = currentX + dForDelta * (-1);
+            currentY = currentY - dForDelta * (-1);
+            printf("%d ", matrix[currentY][currentX]);
+        }
+        dForDelta = CustomPow(-1, i);
     }
-
-    printf("%d\n", matrix[current]);
-
-    for (int i = 0; i < lambda; i++) {
-      current = current - alfa * dForDelta * (-1);
-      printf("%d\n", matrix[current]);
-    }
-    dForDelta = CustomGrade(-1, i);
-  }
-  putchar('\n');
+    putchar('\n');
 }
